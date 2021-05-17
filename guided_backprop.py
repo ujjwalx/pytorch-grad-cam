d1404cba57d61a1613bac2601d21715a66eb6945
Ujjwal Sharma <mailujjwalsharma@gmail.com>
2021-05-17 12:10:05 +0200

    Update GuidedBackprop model.

--- guided_backprop.py
+++ guided_backprop.py
@@ -7,9 +7,7 @@ class GuidedBackpropReLU(Function):
     @staticmethod
     def forward(self, input_img):
         positive_mask = (input_img > 0).type_as(input_img)
-        output = torch.addcmul(
-            torch.zeros(input_img.size()).type_as(input_img), input_img, positive_mask
-        )
+        output = torch.addcmul(torch.zeros(input_img.size()).type_as(input_img), input_img, positive_mask)
         self.save_for_backward(input_img, output)
         return output
 
@@ -20,15 +18,9 @@ class GuidedBackpropReLU(Function):
 
         positive_mask_1 = (input_img > 0).type_as(grad_output)
         positive_mask_2 = (grad_output > 0).type_as(grad_output)
-        grad_input = torch.addcmul(
-            torch.zeros(input_img.size()).type_as(input_img),
-            torch.addcmul(
-                torch.zeros(input_img.size()).type_as(input_img),
-                grad_output,
-                positive_mask_1,
-            ),
-            positive_mask_2,
-        )
+        grad_input = torch.addcmul(torch.zeros(input_img.size()).type_as(input_img),
+                                   torch.addcmul(torch.zeros(input_img.size()).type_as(input_img), grad_output,
+                                                 positive_mask_1), positive_mask_2)
         return grad_input
 
 
@@ -37,25 +29,34 @@ class GuidedBackpropReLUModel:
         self.model = model
         self.model.eval()
         self.cuda = use_cuda
-
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = model.to(device)
-
-        def recursive_relu_apply(module_top):
-            for idx, module in module_top._modules.items():
-                recursive_relu_apply(module)
-                if module.__class__.__name__ == "ReLU":
-                    module_top._modules[idx] = GuidedBackpropReLU.apply
-
-        # replace ReLU with GuidedBackpropReLU
-        recursive_relu_apply(self.model)
+        if self.cuda:
+            self.model = self.model.cuda()
 
     def forward(self, input_img):
         return self.model(input_img)
 
+    def recursive_replace_relu_with_guidedrelu(self, module_top):
+        for idx, module in module_top._modules.items():
+            self.recursive_replace_relu_with_guidedrelu(module)
+            if module.__class__.__name__ == 'ReLU':
+                module_top._modules[idx] = GuidedBackpropReLU.apply
+
+    def recursive_replace_guidedrelu_with_relu(self, module_top):
+        try:
+            for idx, module in module_top._modules.items():
+                self.recursive_replace_guidedrelu_with_relu(module)
+                if module == GuidedBackpropReLU.apply:
+                    module_top._modules[idx] = torch.nn.ReLU()
+        except:
+            pass
+
+
     def __call__(self, input_img, target_category=None):
+        # replace ReLU with GuidedBackpropReLU
+        self.recursive_replace_relu_with_guidedrelu(self.model)
+
         if self.cuda:
-            input_img = input_img.to(self.device)
+            input_img = input_img.cuda()
 
         input_img = input_img.requires_grad_(True)
 
@@ -64,17 +65,15 @@ class GuidedBackpropReLUModel:
         if target_category is None:
             target_category = np.argmax(output.cpu().data.numpy())
 
-        one_hot = np.zeros((1, output.size()[-1]), dtype=np.float32)
-        one_hot[0][target_category] = 1
-        one_hot = torch.from_numpy(one_hot).requires_grad_(True).to(device)
-
-        if self.cuda:
-            one_hot = one_hot.cuda()
-
-        one_hot = torch.sum(one_hot * output)
-        one_hot.backward(retain_graph=True)
+        loss = output[0, target_category]
+        loss.backward(retain_graph=True)
 
         output = input_img.grad.cpu().data.numpy()
         output = output[0, :, :, :]
         output = output.transpose((1, 2, 0))
+
+        # replace GuidedBackpropReLU back with ReLU
+        self.recursive_replace_guidedrelu_with_relu(self.model)
+
         return output
+
